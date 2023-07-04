a32a1e3f75b3d65ffb762c029d8804803edb87a4
tom-brulin <t.brulin@outlook.com>
2023-07-04 09:59:47 +0200

    feat(perms): add basket perm

--- app/Enums/Permissions.ts
+++ app/Enums/Permissions.ts
@@ -1,3 +1,6 @@
 export const Permissions: { [key: string]: string } = {
   PRODUCT_CATEGORY_CREATE: 'product_category.create',
+  BASKET_GET: 'basket.get',
+  BASKET_POST: 'basket.post',
+  BASKET_DELETE: 'basket.delete',
 } as const
--- app/Enums/RoutesPermissions.ts
+++ app/Enums/RoutesPermissions.ts
@@ -17,6 +17,21 @@ const routesPermissions: RoutePermission[] = [
     method: 'POST',
     permission: Permissions.PRODUCT_CATEGORY_CREATE,
   },
+  {
+    route: '/basket',
+    method: 'GET',
+    permission: Permissions.BASKET_GET,
+  },
+  {
+    route: '/basket',
+    method: 'POST',
+    permission: Permissions.BASKET_POST,
+  },
+  {
+    route: '/basket',
+    method: 'DELETE',
+    permission: Permissions.BASKET_DELETE,
+  },
 ]
 
 export const getRoutePermission = (route: string, method: string): string | undefined => {
