b7b9cbe32090cd48a59d0c4937dd540593590fe4
tom-brulin <t.brulin@outlook.com>
2023-07-07 11:55:29 +0200

    feat(db): add seeder for roles permissions

---
+++ app/Enums/RolesPermissions.ts
@@ -0,0 +1,38 @@
+import { Roles } from 'App/Enums/Roles'
+import { Permissions } from 'App/Enums/Permissions'
+
+export const RolesPermissions: { [key: string]: string[] } = {
+  [Roles.ADMIN]: [...Object.values(Permissions)],
+  [Roles.MANAGER]: [
+    Permissions.CATALOG_CATEGORIES_POST,
+    Permissions.CATALOG_CATEGORIES_PUT,
+    Permissions.CATALOG_INGREDIENTS_GET,
+    Permissions.CATALOG_INGREDIENTS_POST,
+    Permissions.CATALOG_INGREDIENTS_PUT,
+    Permissions.CATALOG_PRODUCTS_POST,
+    Permissions.CATALOG_PRODUCTS_PUT,
+    Permissions.CATALOG_PRODUCTS_INGREDIENTS_GET,
+    Permissions.CATALOG_PRODUCTS_INGREDIENTS_POST,
+    Permissions.CATALOG_PRODUCTS_INGREDIENTS_PUT,
+    Permissions.CATALOG_PRODUCTS_INGREDIENTS_DELETE,
+    Permissions.ADDRESSES_ALL,
+    Permissions.DELIVERIES_GET,
+    Permissions.BASKET_GET,
+    Permissions.BASKET_POST,
+    Permissions.BASKET_DELETE,
+  ],
+  [Roles.DELIVERER]: [
+    Permissions.ADDRESSES_ALL,
+    Permissions.DELIVERIES_GET,
+    Permissions.BASKET_GET,
+    Permissions.BASKET_POST,
+    Permissions.BASKET_DELETE,
+  ],
+  [Roles.CUSTOMER]: [
+    Permissions.ADDRESSES_ALL,
+    Permissions.DELIVERIES_GET,
+    Permissions.BASKET_GET,
+    Permissions.BASKET_POST,
+    Permissions.BASKET_DELETE,
+  ],
+}
--- database/seeders/AuthorizationSeeder.ts
+++ database/seeders/AuthorizationSeeder.ts
@@ -1,6 +1,8 @@
+import Database from '@ioc:Adonis/Lucid/Database'
 import BaseSeeder from '@ioc:Adonis/Lucid/Seeder'
 import { Permissions } from 'App/Enums/Permissions'
 import { Roles } from 'App/Enums/Roles'
+import { RolesPermissions } from 'App/Enums/RolesPermissions'
 import Permission from 'App/Models/Permission'
 import Role from 'App/Models/Role'
 
@@ -41,17 +43,42 @@ export default class extends BaseSeeder {
   }
 
   private async createRolePermissions() {
+    const adminRole = await Role.findOrFail(Roles.ADMIN)
+    const managerRole = await Role.findOrFail(Roles.MANAGER)
+    const delivererRole = await Role.findOrFail(Roles.DELIVERER)
     const customerRole = await Role.findOrFail(Roles.CUSTOMER)
+
+    if (adminRole) {
+      await this.createPermissionsForRole(adminRole.id, RolesPermissions[Roles.ADMIN])
+    }
+
+    if (managerRole) {
+      await this.createPermissionsForRole(managerRole.id, RolesPermissions[Roles.MANAGER])
+    }
+
+    if (delivererRole) {
+      await this.createPermissionsForRole(delivererRole.id, RolesPermissions[Roles.DELIVERER])
+    }
+
     if (customerRole) {
+      await this.createPermissionsForRole(customerRole.id, RolesPermissions[Roles.CUSTOMER])
+    }
+  }
+
+  private async createPermissionsForRole(roleId: string, permissions: string[]) {
+    for (const permission of permissions) {
       if (
-        !(await customerRole
-          .related('permissions')
-          .query()
-          .where('permissions.id', Permissions.PRODUCT_CATEGORY_CREATE))
+        (
+          await Database.query()
+            .from('role_permission')
+            .where('role_id', roleId)
+            .where('permission_id', permission)
+        ).length <= 0
       ) {
-        await customerRole
-          .related('permissions')
-          .createMany([{ id: Permissions.PRODUCT_CATEGORY_CREATE }])
+        await Database.insertQuery().table('role_permission').insert({
+          role_id: roleId,
+          permission_id: permission,
+        })
       }
     }
   }
